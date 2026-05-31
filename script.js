44d8b2dc8251692e7eb720e421675c8cf6494d2b
dishanchowdhury111-creator <dishanchowdhury111@gmail.com>
2026-05-31 12:07:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,57 @@ function logout(){
     localStorage.removeItem("user");
     window.location.href = "index.html";
 }
+// Attendance storage
+let attendanceData = JSON.parse(localStorage.getItem("attendance")) || {};
+
+function loadAttendance(){
+    const date = document.getElementById("attDate").value;
+
+    if(!date){
+        alert("Select a date");
+        return;
+    }
+
+    const list = document.getElementById("attendanceList");
+    list.innerHTML = "";
+
+    const saved = attendanceData[date] || {};
+
+    students.forEach(stu => {
+        const status = saved[stu.id] || "absent";
+
+        list.innerHTML += `
+            <div class="att-item">
+                <div>
+                    <b>${stu.name}</b> (Class: ${stu.class})
+                </div>
+
+                <select id="att-${stu.id}">
+                    <option value="present" ${status === "present" ? "selected" : ""}>Present</option>
+                    <option value="absent" ${status === "absent" ? "selected" : ""}>Absent</option>
+                </select>
+            </div>
+        `;
+    });
+}
+
+function saveAttendance(){
+    const date = document.getElementById("attDate").value;
+
+    if(!date){
+        alert("Select a date first");
+        return;
+    }
+
+    const record = {};
+
+    students.forEach(stu => {
+        const value = document.getElementById(`att-${stu.id}`).value;
+        record[stu.id] = value;
+    });
+
+    attendanceData[date] = record;
+    localStorage.setItem("attendance", JSON.stringify(attendanceData));
+
+    alert("Attendance saved!");
+}
